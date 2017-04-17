a6d38a9edc0cd6277ae8d79d3e79aea28eff5bd0
Daniel A.C. Martin <daniel-ac-martin@users.noreply.github.com>
2017-04-17 18:54:03 +0100

    WIP: Add support for environment variables

--- src/Settings/Applicative.hs
+++ src/Settings/Applicative.hs
@@ -1,6 +1,7 @@
 module Settings.Applicative ( module Control.Applicative
                             , Parser
                             , auto
+                            , envvar
                             , execParser
                             , fullDesc
                             , header
@@ -36,3 +37,12 @@ import Options.Applicative ( Parser
                            , strOption
                            , switch
                            , value )
+
+import Options.Applicative.Builder.Internal ( DefaultProp(..)
+                                            , HasValue
+                                            , Mod(..) )
+
+-- Just a clone of 'value' for now
+-- We will need to do IO so need to think about how to orchestrate that
+envvar :: HasValue f => a -> Mod f a
+envvar x = Mod id (DefaultProp (Just x) Nothing) id
